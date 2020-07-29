539f2bf57956af78bc89c5a4eddf2f475df3666c
Chandugr <chandu30bca@gmail.com>
2020-07-29 17:38:15 +0530

    Updates

---
+++ precache-manifest.2b6f4aff34889f8b5e7fc93e003231f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54a60a0e3065a9f6b48b931dfa0b845",
+    "url": "/ReactJS-Portfolio/index.html"
+  },
+  {
+    "revision": "2da086e08fce3b8f6abe",
+    "url": "/ReactJS-Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "efc238b7f69494d8138a",
+    "url": "/ReactJS-Portfolio/static/css/main.7dc6eddb.chunk.css"
+  },
+  {
+    "revision": "2da086e08fce3b8f6abe",
+    "url": "/ReactJS-Portfolio/static/js/2.ab242305.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/ReactJS-Portfolio/static/js/2.ab242305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc238b7f69494d8138a",
+    "url": "/ReactJS-Portfolio/static/js/main.b95a9e3f.chunk.js"
+  },
+  {
+    "revision": "6bb8b60c545d84eea536",
+    "url": "/ReactJS-Portfolio/static/js/runtime-main.4be7fcf8.js"
+  }
+]);
